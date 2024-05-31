304bba989f1d529124a2fa6a7ae6f56ee30977b4
Matheus Ribeiro <158311367+DevMrn@users.noreply.github.com>
2024-05-30 23:36:13 -0300

    Add files via upload

---
+++ controle/WhileIndeterminado.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String valor = "";
+		
+		while(!valor.equalsIgnoreCase("sair")) {
+			System.out.print("Você diz: ");
+			valor = entrada.nextLine();
+		}
+		
+		entrada.close();
+	}
+}
+
+	// WHILE para números indeterminados
+	// FOR para números determinados
